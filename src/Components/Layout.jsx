e2f75b066c3fbd09a90f6938c7b1096aa786e25d
Fjodor <id19fjs>
2024-09-05 20:56:48 +0200

    Initial commit

--- src/Components/Layout.jsx
+++ src/Components/Layout.jsx
@@ -4,10 +4,12 @@ import Header from './Header';
 import Menu from './Menu';
 import Banner from './Banner';
 import Footer from './Footer';
+import { Helmet } from "react-helmet";
 
 const Layout = () => {
   return (
     <>
+    <Helmet><author>Fjodor Joss, fjodor_joss@hotmail.com</author></Helmet>
       <Header/>
       <Banner/>
       <Outlet />
